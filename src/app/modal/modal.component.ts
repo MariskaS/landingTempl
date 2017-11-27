9745b24775916eadf6d87a3ec7692a1330c24675
Marina Ismailova <marina_ismailova@epam.com>
2017-11-27 22:27:32 +0300

    master: fix template & use less

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -3,7 +3,7 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  styleUrls: ['./modal.component.less']
 })
 export class ModalComponent implements OnInit {
   constructor() { }
